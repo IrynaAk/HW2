b21c362fa0d0d51d2f0821fbbb1f769a99d5fa8f
Iryna_Akunevich <akunevichirina@mail.ru>
2022-06-13 18:15:01 +0300

    Check if a day is the day off

---
+++ Task02/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+//и проверяет, является ли этот день выходным.
+
+using System;
+using static System.Console;
+Clear();
+
+WriteLine("Please type a number from 1 to 7");
+int number = Convert.ToInt32(ReadLine());
+
+if ((number<1) || (number>7)) {WriteLine($"{number} - this is not a number from 1 to 7");
+                                    return;}
+
+WriteLine(number==6 || number==7? $"This is a day off"
+: $"This is a working day");
