07bb9520f00778d65038c9d4100a6ce7032aa5b7
Iryna_Akunevich <akunevichirina@mail.ru>
2022-06-13 18:01:15 +0300

    3rd digit of the number

---
+++ Task01/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+using System;
+using static System.Console;
+Clear();
+
+WriteLine("Please type a number");
+int number = Convert.ToInt32(ReadLine());
+
+WriteLine(Math.Abs((number%1000)/100)>0? $"The third digit is {Math.Abs((number%1000)/100)}"
+: $"There is no third digit in the number");
